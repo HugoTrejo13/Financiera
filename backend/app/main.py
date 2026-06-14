4c66f2fc2d63d9f1d9c1f9266d277bda138a4cc1
Hugo E. Trejo de la Mora <supern727@gmail.com>
2026-06-13 22:50:00 -0600

    fix servidor con bug

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,15 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:5174", 
+        "http://localhost:5175", 
+        "http://localhost:5176", 
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+        "http://127.0.0.1:5175",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
